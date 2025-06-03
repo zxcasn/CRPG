fbb249bc96e22833ac7e47649e924962f290e365
ASANO <72429665+zxcasn@users.noreply.github.com>
2025-06-03 18:25:53 +0900

    Setup Vite project files

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CardDeckRPG from './init'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <CardDeckRPG />
+  </React.StrictMode>,
+)
